84687cb510208d249f5a22a678b703fc91351f6f
andea13 <110096764+andea13@users.noreply.github.com>
2023-05-18 20:44:28 +0000

    Deploying to gh-pages from @ andea13/goit-js-hw-10@990486690c91da4d08c5aa487a09dedc0058e24d 🚀

---
+++ index.abf1a632.js
@@ -0,0 +1,2 @@
+console.log(fetch("".concat("https://restcountries.com/v3.1","/all?fields=name,capital,population,flags,languages")).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()})));
+//# sourceMappingURL=index.abf1a632.js.map
--- index.ae220f46.js
+++
@@ -1,2 +0,0 @@
-!function(){var t;console.log((t="Ukraine",fetch("".concat("https://restcountries.com/v3.1","/name/").concat(t)).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()}))))}();
-//# sourceMappingURL=index.ae220f46.js.map
