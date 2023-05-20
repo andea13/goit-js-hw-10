e5d351da8ce2b55c821309f849d9fd0970fa037a
andea13 <110096764+andea13@users.noreply.github.com>
2023-05-20 13:09:38 +0000

    Deploying to gh-pages from @ andea13/goit-js-hw-10@fdd5515c2d2fcb7cc3591e9624af1fe9d4b7704c 🚀

--- index.043cf698.js
+++
@@ -1,2 +0,0 @@
-console.log(fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages").then((t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})));
-//# sourceMappingURL=index.043cf698.js.map
--- index.abf1a632.js
+++
@@ -1,2 +0,0 @@
-console.log(fetch("".concat("https://restcountries.com/v3.1","/all?fields=name,capital,population,flags,languages")).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()})));
-//# sourceMappingURL=index.abf1a632.js.map
